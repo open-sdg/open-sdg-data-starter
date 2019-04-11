2a2f4f80df7eb6de5c16c65c0ab8d7b56f293db2
kz-userstat <49387961+kz-userstat@users.noreply.github.com>
2019-04-11 11:34:12 +0600

    Update deploy_prod.sh

--- scripts/deploy/circleci/deploy_prod.sh
+++ scripts/deploy/circleci/deploy_prod.sh
@@ -20,7 +20,7 @@ git config --global user.name "$GH_NAME"
 # CircleCI will identify the SSH key with a "Host" of gh-prod. In order to tell
 # Git to use this key, we need to hack the SSH key:
 sed -i -e 's/Host gh-prod/Host gh-prod\n  HostName github.com/g' ~/.ssh/config
-git clone git@gh-prod:$GH_ORG_PROD/$CIRCLE_PROJECT_REPONAME.git out
+git clone git@gh-prod:$GH_ORG_PROD/sdg-data-kazstat-prod.git out
 
 cd out
 git checkout gh-pages || git checkout --orphan gh-pages
