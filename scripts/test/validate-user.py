d27746ae4f0983902f6503134a32592ce9958d97
Brock Fanning <brockfanning@gmail.com>
2019-05-28 22:04:45 -0400

    Fix path again

--- scripts/test/validate-user.py
+++ scripts/test/validate-user.py
@@ -1,3 +1,3 @@
 from gitgang.github import members_only
 
-members_only(github_org='kazstat', github_repo='sdg-data-kazstat', gangfile='scripts/tests/github-users.yml')
+members_only(github_org='kazstat', github_repo='sdg-data-kazstat', gangfile='scripts/test/github-users.yml')
