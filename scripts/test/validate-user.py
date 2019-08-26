ad1ad53b29ae46e16bf6b47092a22bca5baf36c1
Brock Fanning <brockfanning@gmail.com>
2019-08-26 17:38:59 -0400

    Add missing param for user validation

--- scripts/test/validate-user.py
+++ scripts/test/validate-user.py
@@ -1,3 +1,8 @@
 from gitgang.github import members_only
 
-members_only(github_org='kazstat', github_repo='sdg-data-kazstat', gangfile='scripts/test/github-users.yml')
+members_only(
+  github_org='kazstat',
+  github_repo='sdg-data-kazstat',
+  git_branch='develop',
+  gangfile='scripts/test/github-users.yml'
+)
